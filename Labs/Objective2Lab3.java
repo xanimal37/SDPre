13694af5abc452ccae8ac2e28de4c8dbea6aa734
xanimal37 <hawkbit37@gmail.com>
2022-12-26 19:03:08 -0600

    Obj2 Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+    int year = 1978;
+    String month = "December";
+    boolean isWeekend = false;
+
+    System.out.println("Year: " + year);
+    System.out.println("Month: " + month);
+    System.out.println("Weekend: " + isWeekend);
+  }
+}
