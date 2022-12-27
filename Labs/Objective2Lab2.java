a5136fafbdb8fc49efc1447affbf0a91cf4b696e
xanimal37 <hawkbit37@gmail.com>
2022-12-26 18:56:19 -0600

    obj2 lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
