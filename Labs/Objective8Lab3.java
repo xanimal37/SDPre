a59ad5a89a7d8e8ec55dbcd95009d7f5795f8e42
xanimal37 <hawkbit37@gmail.com>
2022-12-27 21:56:10 -0600

    Obj8 Lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,15 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+    
+    for(int i=1;i<21;i++){
+      if(i%2==0){
+        System.out.println(i + " is even.");
+      }
+      else {
+        System.out.println(i + " is odd.");
+      }
+    }
+
+    
+  }
+}
