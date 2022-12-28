fc1462bea7f5e9eb9c5b44b7541f813ebe2136e6
xanimal37 <hawkbit37@gmail.com>
2022-12-27 21:40:32 -0600

    Obj7 Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,13 @@
+public class Objective7Lab4 {
+	public static void main(String[] args){
+		int counter=1;
+		int total=0;
+
+		while(counter<=20){
+			total = total + counter;
+			counter++;
+		}
+
+		System.out.println(total);
+	}
+}
